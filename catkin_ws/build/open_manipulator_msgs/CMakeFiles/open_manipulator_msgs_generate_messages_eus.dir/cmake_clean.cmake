9ccfe656ce2a4952cca0a49c136b68dd1dd146af
aadityanimkar <aaditya.nimkar@gmail.com>
2024-10-30 23:38:20 +0530

    open manipulator setup

---
+++ catkin_ws/build/open_manipulator_msgs/CMakeFiles/open_manipulator_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/manifest.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/msg/JointPosition.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/msg/KinematicsPose.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/msg/OpenManipulatorState.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/srv/GetJointPosition.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/srv/GetKinematicsPose.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/srv/SetActuatorState.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/srv/SetDrawingTrajectory.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/srv/SetJointPosition.l"
+  "/home/aaditya/ITR/Open_manipulator/catkin_ws/devel/share/roseus/ros/open_manipulator_msgs/srv/SetKinematicsPose.l"
+  "CMakeFiles/open_manipulator_msgs_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/open_manipulator_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/devel/share/gennodejs/ros/open_manipulator_msgs/srv/_index.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+let SetActuatorState = require('./SetActuatorState.js')
+let GetKinematicsPose = require('./GetKinematicsPose.js')
+let GetJointPosition = require('./GetJointPosition.js')
+let SetDrawingTrajectory = require('./SetDrawingTrajectory.js')
+let SetKinematicsPose = require('./SetKinematicsPose.js')
+let SetJointPosition = require('./SetJointPosition.js')
+
+module.exports = {
+  SetActuatorState: SetActuatorState,
+  GetKinematicsPose: GetKinematicsPose,
+  GetJointPosition: GetJointPosition,
+  SetDrawingTrajectory: SetDrawingTrajectory,
+  SetKinematicsPose: SetKinematicsPose,
+  SetJointPosition: SetJointPosition,
+};
