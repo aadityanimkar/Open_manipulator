36cf1ec677ed140dc464bddd5ad447a1146f1e2a
Vardan <vardanmittal000@gmail.com>
2024-11-13 22:21:20 +0530

    some updates

--- catkin_ws/build/tal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aaditya/ITR/Open_manipulator/catkin_ws/src/tal
-# Build directory: /home/aaditya/ITR/Open_manipulator/catkin_ws/build/tal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
